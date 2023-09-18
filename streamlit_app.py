fadf592b2f692de3fa53cdfa7a9c0c5cf9fc8545
gpddizon <145098508+gpddizon@users.noreply.github.com>
2023-09-18 22:52:07 +0800

    Update streamlit_app.py
    Updated section to display fruity vice api resp

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,22 +25,23 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+############################################################
 # New Section to display fruity vice API response
 streamlit.header('Fruityvice Fruit Advice!')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlity.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-# Added section to select fruit choices
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi') 
-streamlit.write('The user entered ', fruit_choice)
+except URLError as e:
+  streamliy.error()
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# Commenting out Display Plain JSON text
-# streamlit.text(fruityvice_response.json())
+#######################################################################
 
-# Normalize JSON return from FruityVice response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Display return in a table
-streamlit.dataframe(fruityvice_normalized)
 
 #do not run anything past here while we troubleshoot
 streamlit.stop()
